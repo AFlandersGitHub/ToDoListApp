641fa41d27526d0fa9308c7dc65c9c82fcbcd092
AFlandersGitHub <103605423+AFlandersGitHub@users.noreply.github.com>
2025-03-09 14:56:11 -0400

    created second bug fixing exercise and completed

---
+++ exercises/bugfixingexercise2.py
@@ -0,0 +1,14 @@
+#Bugfixingexercise- Fix the created bugs
+
+#Bugfixingexercise1 Prompt Below
+#while True:
+    #print("Hello")
+
+
+#Bug-Fixing Exercise 2 (infinite loop but append each input country into list)
+countries = []
+
+while True:
+    country = input("Enter the country: ")
+    countries.append(country)
+    print(countries)
