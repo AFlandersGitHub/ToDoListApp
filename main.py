c52eaa50d41f6c671910cb9fb5c07155929dd5cd
AFlandersGitHub <103605423+AFlandersGitHub@users.noreply.github.com>
2025-07-01 19:17:27 -0400

    custom function get_todos() and test.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def get_todos():
+    with open('todos.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -5,18 +11,16 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
-    elif user_action.startswith('show'):
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
 
-        # new_todos = [item.strip('\n') for item in todos]
+    elif user_action.startswith('show'):
+
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -29,8 +33,7 @@ while True:
 
             number = number - 1
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
@@ -45,8 +48,7 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
             index = number -1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
